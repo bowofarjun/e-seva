0281b14a70ea2ec7ea00c663f5816414b6a6cd7a
Mishra, Arjun <bowofarjun@gmail.com>
2022-10-15 04:40:00 +0530

    Initial springboot with jpa and security configurtion for ms sql server connectivity

---
+++ API/eseva-api/src/test/java/in/ac/bitspilani/wilp/esevaapi/EsevaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ac.bitspilani.wilp.esevaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsevaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
